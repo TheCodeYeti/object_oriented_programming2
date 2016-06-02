c976a920418180ed7389a569c6813807f6eb4913
spencer <spencer.dossett@gmail.com>
2016-06-02 14:11:01 -0400

    added initialize method for Cat class

--- cat.rb
+++ cat.rb
@@ -1,5 +1,9 @@
 class Cat
   attr_reader :name
   attr_accessor :perferred_food, :meal_time
-  
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @perferred_food = perferred_food
+    @meal_time = meal_time
+  end
 end
