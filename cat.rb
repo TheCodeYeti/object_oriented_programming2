a59393daf3d08377a7ab31d0e839618a2d66c5c5
spencer <spencer.dossett@gmail.com>
2016-06-02 14:23:14 -0400

    added meow method to class Cat

--- cat.rb
+++ cat.rb
@@ -6,7 +6,21 @@ class Cat
     @perferred_food = perferred_food
     @meal_time = meal_time
   end
+  def eats_at
+    if meal_time > 12
+      display_time = meal_time
+      am_pm = "AM"
+    else
+      display_time = meal_time - 12
+      am_pm = "PM"
+    end
+    "#{display_time} #{am_pm}"
+  end
+  def meow
+    "My name is #{name} and I eat #{preferred_food} at #{meal_time}"
+  end
+
 end
 
-latte = Cat.new("Latte", "Chicken", "12")
+latte = Cat.new("Latte", "Chicken", "13")
 harley = Cat.new("Harley", "Whatever is in #{latte.name}'s bowl", "8")
