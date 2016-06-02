34f922a7e358309537ced4db50056746d3e70ab1
spencer <spencer.dossett@gmail.com>
2016-06-02 14:33:35 -0400

    bug fixes around variable names being misspelled

--- cat.rb
+++ cat.rb
@@ -1,26 +1,28 @@
 class Cat
   attr_reader :name
-  attr_accessor :perferred_food, :meal_time
+  attr_accessor :preferred_food, :meal_time
   def initialize(name, preferred_food, meal_time)
     @name = name
-    @perferred_food = perferred_food
-    @meal_time = meal_time
+    @preferred_food = preferred_food
+    @meal_time = meal_time.to_i
   end
   def eats_at
-    if meal_time > 12
-      display_time = meal_time
+    if @meal_time <= 12
+      display_time = @meal_time
       am_pm = "AM"
     else
-      display_time = meal_time - 12
+      display_time = @meal_time - 12
       am_pm = "PM"
     end
     "#{display_time} #{am_pm}"
   end
   def meow
-    "My name is #{name} and I eat #{preferred_food} at #{meal_time}"
+    "My name is #{@name} and I eat #{@preferred_food} at #{eats_at}"
   end
 
 end
 
 latte = Cat.new("Latte", "Chicken", "13")
 harley = Cat.new("Harley", "Whatever is in #{latte.name}'s bowl", "8")
+puts latte.meow
+puts harley.meow
