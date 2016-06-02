d56d0db2683940f815366e036017139b678a65cb
spencer <spencer.dossett@gmail.com>
2016-06-02 14:51:57 -0400

    Added quota method to Paperboy class

--- paperboy.rb
+++ paperboy.rb
@@ -1,6 +1,13 @@
 class Paperboy
   attr_reader :name
-  attr_accessor :quota, :experience, :side
+  attr_accessor :experience, :side
   attr_reader :earnings
 
+  def initialize(name, quota = 0, experience = 0, side = "even", earnings = 0 )
+
+
+  end
+  def quota
+    @quota
+  end
 end
