86fe958bd7a6114abf5ceec4a848dc766b3e6af1
spencer <spencer.dossett@gmail.com>
2016-06-02 15:37:39 -0400

    Added calculate_earnings method to Paperboy

--- paperboy.rb
+++ paperboy.rb
@@ -20,19 +20,30 @@ class Paperboy
     total_houses = actual_end - actual_start + 1
     papers_delivered = (total_houses / 2.0)
     if actual_start.even? && actual_end.even?
-      if side = "even"
+      if side == "even"
         papers_delivered = papers_delivered.ceil
       else
         papers_delivered = papers_delivered.floor
       end
     elsif actual_start.odd? && actual_end.odd?
-      if side = "odd"
+      if side == "odd"
         papers_delivered = papers_delivered.ceil
       else
         papers_delivered = papers_delivered.floor
       end
     end
-    @experience = papers_delivered
+    @experience += papers_delivered
+    calculate_earnings(papers_delivered)
 
   end
+  def calculate_earnings(papers_delivered)
+    papers_within_quota = [papers_delivered, @quota].min
+    quota_missed = [@quota - papers_delivered, 0].max
+    papers_above_quota = papers_delivered - papers_within_quota
+    @earnings += (papers_within_quota * 0.25).to_f
+    @earnings += (papers_above_quota * 0.50).to_f
+    @earnings += (quota_missed * 2.00)).to_f
+    @earnings
+  end
+
 end
