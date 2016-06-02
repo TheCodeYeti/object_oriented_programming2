51aea8df61f38020588ac95f24910426ec78eee8
spencer <spencer.dossett@gmail.com>
2016-06-02 14:07:45 -0400

    Created class Cat

--- cat.rb
+++ cat.rb
@@ -1,26 +1,3 @@
-class Person
-  attr_reader :name
-  def initialize(name)
-    @name = name
-  end
-  def greeting
-    "Hi, my name is #{name}"
-  end
-end
+class Cat
 
-class Student < Person
-  def learn
-    "I get it!"
-  end
 end
-
-class Instructor < Person
-  def teach
-    "Everything in Ruby is an Object"
-  end
-end
-
-chris = Instructor.new("Chris")
-christina = Student.new("Christina")
-puts chris.teach
-puts christina.learn
